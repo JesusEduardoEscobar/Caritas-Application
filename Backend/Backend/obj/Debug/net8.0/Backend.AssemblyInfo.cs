6b8bd3f6be89ab3c39cf2d032b4c92762c4eca07
JesusEduardoEscobar <programjesuse@gmail.com>
2025-10-07 18:18:41 -0600

    25.10.07.01 correcciones del git

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd57dba2f089c9f8941b806c88e37c0bb3e7864a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8699a2046e35a00467be5cb971d4c73205262a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
