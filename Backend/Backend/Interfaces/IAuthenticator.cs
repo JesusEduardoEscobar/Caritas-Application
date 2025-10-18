e40dcb3f9d0a553bc6a386220f1db3c7d5e384c6
JesusEduardoEscobar <programjesuse@gmail.com>
2025-10-17 18:26:25 -0600

    25.10.17.01 separacion del los usuarios

--- Backend/Backend/Interfaces/IAuthenticator.cs
+++ Backend/Backend/Interfaces/IAuthenticator.cs
@@ -10,6 +10,12 @@ namespace Backend.Interfaces
     public interface IAuthenticator
     {
         Task<GlobalResponse<dynamic>> Login(string email, string password);
+        // REGISTRO DE LOS USUARIOS
+        Task<GlobalResponse<dynamic>> RegisterLite(string nombre, string password, string numero);
+        Task<GlobalResponse<dynamic>> RegisterUser(string nombre, string email, string password, string numero, string nivelEconomico, bool verifacion);
+        Task<GlobalResponse<dynamic>> RegisterAdmin(string name, string email, string password, string emailAdmin, string passwordAdmin);
+        // CHECAR QUE LOS USUARIOS SI ESTEN VERIFICADOS
+        Task<GlobalResponse<dynamic>> VerifyUser(int id, bool verificacion);
 
     }
 }
