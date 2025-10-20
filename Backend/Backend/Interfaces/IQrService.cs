27db1db7d9afdc1c76b135f1241255105c946874
IngeMoreno23 <a01742930@tec.mx>
2025-10-20 11:57:18 -0600

    ff node modules

---
+++ Backend/Backend/Interfaces/IQrService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Infraestructure.Implementations;
+using Backend.Infraestructure.Models;
+
+namespace Backend.Interfaces
+{
+    public interface IQrService
+    {
+        Task<GlobalResponse<dynamic>> ReadQr(string qr);
+
+        Task<GlobalResponse<string>> GenerateReservationQr(int id);
+        Task<GlobalResponse<string>> GenerateServiceReservationQr(int id);
+        Task<GlobalResponse<string>> GenerateTransportRequestQr(int id);
+    }
+}
